7b21d3d0ea0cafee76480ba45ba8de946aa85562
Akbar <shyar.akbar@gmail.com>
2025-06-13 11:43:36 +0500

    just changed the allowd hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4yi_g4i%rfm!5%koz40=k&_4_tgxeo$c)j+7+x&%u-vw(is1ue
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
